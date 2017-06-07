84bbeca57f43d13c6d95758522203e9d9bb1f16a
Gpx <polvara@gmail.com>
2017-06-07 22:11:29 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const calculateExpectedGMV = () => {
+  const amount = parseFloat(input.value)
+  const now = new Date()
+  const currentTime = now.getTime()
+  const beginOfMonth = new Date(now.getFullYear(), now.getMonth(), 1).getTime()
+  const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0).getTime()
+
+  return amount * (endOfMonth - beginOfMonth) / (currentTime - beginOfMonth)
+}
+
+const updateAmount = () => {
+  const expectedGMV = calculateExpectedGMV()
+  output.innerText = expectedGMV.toLocaleString('en-US', { style: 'currency', currency: 'EUR' })
+
+  setTimeout(updateAmount, 300)
+}
+
+const input = document.getElementById('income')
+const output = document.getElementById('expected')
+
+updateAmount()
